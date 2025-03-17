2c79b5f5a3561283b82b5092eaa882bbc4ed5f4b
Eamon <2460574924@qq.com>
2025-03-17 18:13:17 +0800

    feat: 手写effect初始化

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,1 +1,11 @@
-export function effect() {}
+export function effect(fn, options?) {
+  // 创建一个响应式effect，数据变化后可以重新执行
+
+  // 创建一个effect，只要依赖的属性变化了，就要重新执行回调
+  const _effect = new ReactiveEffect(fn, () => {
+    _effect.run();
+  });
+
+  // 默认先执行一次
+  _effect.run();
+}
