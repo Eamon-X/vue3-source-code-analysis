6e530a8aac72a9590ddd999aa8ab68115c4d279e
Eamon <2460574924@qq.com>
2025-03-19 00:05:27 +0800

    feat: 实现依赖收集一

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -12,6 +12,7 @@ export function effect(fn, options?) {
 }
 export let activeEffect; // 解决嵌套effect时的依赖关系混乱的问题
 class ReactiveEffect {
+  _trackId = 0; // 用于记录当前effect执行了几次
   public active = true; // 控制创建的effect是否是响应式的，effectScope.stop() 停止所有的effect 不参加响应式处理
   // fn是用户编写的函数
   // 如果fn中依赖的数据发生变化后，就重新调用scheduler，即重新执行run函数
@@ -36,4 +37,19 @@ class ReactiveEffect {
   stop() {
     /* 从所有依赖中移除自身 */
   }
+}
+
+/**
+ * 将当前的 effect 添加到依赖集合中，用于建立响应式关系。
+ *
+ * @param effect 当前的 ReactiveEffect 实例，表示需要被追踪的副作用函数。
+ * @param dep 依赖集合（通常是一个 WeakMap 或 Map 的值），用于存储所有相关的 effect。
+ *
+ * 功能：
+ * - 将 effect 和其对应的唯一标识 `_trackId` 记录到依赖集合 `dep` 中。
+ * - 确保当依赖的数据发生变化时，能够通过 `dep` 找到并触发相关的 effect。
+ * - 使用 `dep.set(effect, effect._trackId)` 来记录 effect 的执行次数或状态，避免重复添加相同的 effect。
+ */
+export function trackEffect(effect, dep) {
+  dep.set(effect, effect._trackId);
 }
--- packages/reactivity/src/reactiveEffect.ts
+++ packages/reactivity/src/reactiveEffect.ts
@@ -1,13 +1,44 @@
 import { activeEffect } from "./effect";
 
+const targetMap = new WeakMap(); // 存放依赖收集的关系
+
+/**
+ * 用于创建Map
+ * @param cleanup 这个是清理方法，用于清理不需要的属性
+ * @param key 标记当前map属于哪个属性（源码没有）
+ * @returns
+ */
+export const createDep = (cleanup, name) => {
+  const dep = new Map() as any;
+  dep.cleanup = cleanup;
+  dep.name = name;
+  return dep;
+};
 export function track(target, key) {
   // 有这个属性，证明这个key是在effect中访问的，没有则说明是在effect之外访问的，不用收集
   if (activeEffect) {
     console.log("track", target, key, activeEffect);
+
+    let depsMap = targetMap.get(target);
+    if (!depsMap) {
+      // 没有收集过该对象，需要新增
+      // 第一个参数 target 是键，通常代表一个被代理的对象；第二个参数就是 (depsMap = new Map()) 表达式的值，即新创建的 Map 对象。
+      // (depsMap = new Map())这是一个赋值表达式，它先创建一个新的 Map 对象，接着把这个新对象赋值给变量 depsMap。赋值表达式的值就是赋值操作的右值，也就是新创建的 Map 对象。所以，这个表达式的值就是新创建的 Map 对象。
+      targetMap.set(target, (depsMap = new Map()));
+    }
+
+    // 判断当前映射表是否存在当前属性，没有则新增
+    let dep = depsMap.get(key);
+    if (!dep) {
+      depsMap.set(key, (dep = createDep(() => depsMap.delete(key), key))); // 后面用于清理不需要的属性
+    }
+
+    trackEffect(activeEffect, dep); // 将当前的effect放到dep（映射表）中，后续可以根据值的变化触发此dep中存放的effect
   }
 }
 
 // 需要记录的格式，记录每个属性收集的effect
+// tarMap: {obj: {key: Map:{effectFn1, effectFn2}}
 /*
 {
     { name: "jw", age: 30 }: {
